77dc9129ebe373dba6562e3a2b2bc08bf703bcb4
Aries <urchinzhou@gmail.com>
2023-08-16 19:51:37 +0800

    update xml assignment

---
+++ extract_data_from_xml.py
@@ -0,0 +1,32 @@
+import urllib.request, urllib.parse, urllib.error
+import xml.etree.ElementTree as ET
+import ssl
+
+api_key = False
+# If you have a Google Places API key, enter it here
+# api_key = 'AIzaSy___IDByT70'
+# https://developers.google.com/maps/documentation/geocoding/intro
+
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/xml?'
+else :
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/xml?'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+uh = urllib.request.urlopen("http://py4e-data.dr-chuck.net/comments_1872140.xml", context=ctx)
+
+data = uh.read()
+print('Retrieved', len(data), 'characters')
+print(data.decode())
+tree = ET.fromstring(data)
+
+sum = 0
+results = tree.findall("./comments/comment/count")
+for item in results:
+    sum = sum + int(item.text)
+print(sum)
